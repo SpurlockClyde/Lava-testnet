1559c360eb59faae8400980d5b8762d47fbb901b
Aazzu5 <161419275+Aazzu5@users.noreply.github.com>
2024-06-11 20:44:15 +0100

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-00c6ad4a-5ae1-4fcd-b0ab-703adcd0635e/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-d3a0c45b-9f0b-4b1e-8ec3-e875362d42b7/"
 
 OUTPUT_DIR="logs"
 
